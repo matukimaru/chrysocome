40b5e0cc6f1cb23491af87148275b8ee9ee05034
matukimaru <matu.kimaru@gmail.com>
2023-12-09 21:37:45 +0300

    add pages and layout directories.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <UCard :ui="ui" />
+  <div class="test-sm">Hello</div>
 </template>
 
 <script setup>
 const ui = /*ui*/ {
-  background: 'bg-white dark:bg-slate-900'
+  background: 'bg-white dark:bg-slate-600'
 }
 </script>
